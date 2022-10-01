d8448a3054c6aad43fb8b9575d3c87839639f1d7
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-01 10:39:07 -0500

    update app and models

--- app/app.py
+++ app/app.py
@@ -14,46 +14,20 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 @app.route('/')
-def index():
+def home():
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    animal = Animal.query.filter(Animal.id == id).first()
-    response_body = f''
-    response_body += f'<ul>ID: {animal.id}</ul>'
-    response_body += f'<ul>Name: {animal.name}</ul>'
-    response_body += f'<ul>Species: {animal.species}</ul>'
-    response_body += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
-    response_body += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
-
-    return make_response(response_body)
+    return ''
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
-    response_body = f''
-    response_body += f'<ul>ID: {zookeeper.id}</ul>'
-    response_body += f'<ul>Name: {zookeeper.name}</ul>'
-    response_body += f'<ul>Birthday: {zookeeper.birthday}</ul>'
-
-    for animal in zookeeper.animals:
-        response_body += f'<ul>Animal: {animal.name}</ul>'
-
-    return make_response(response_body)
+    return ''
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
-    response_body = f''
-    response_body += f'<ul>ID: {enclosure.id}</ul>'
-    response_body += f'<ul>Environment: {enclosure.environment}</ul>'
-    response_body += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
-
-    for animal in enclosure.animals:
-        response_body += f'<ul>Animal: {animal.name}</ul>'
-
-    return make_response(response_body)
+    return ''
 
 
 if __name__ == '__main__':
